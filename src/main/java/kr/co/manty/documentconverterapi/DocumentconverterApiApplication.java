9867c9200e32e9aba2f699a814c207a04ce9ec67
Manty <jibum.jung@gmail.com>
2026-01-25 21:26:44 +0900

    first commit

---
+++ src/main/java/kr/co/manty/documentconverterapi/DocumentconverterApiApplication.java
@@ -0,0 +1,13 @@
+package kr.co.manty.documentconverterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentconverterApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentconverterApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/manty/documentconverterapi/DocumentconverterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.manty.documentconverterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentconverterApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
